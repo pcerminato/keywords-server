8fba7ba58cfea5ff8a2dd22614944cbae179dcf7
pcerminato@gmail.com <pcerminato@gmail.com>
2026-05-27 14:49:00 -0300

    chore: Configs jest to esmodule

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,18 @@
-const { createDefaultPreset } = require("ts-jest");
-
-module.exports = {
+export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'js'],
   testMatch: ['**/tests/**/*.test.(ts|js)', '**/*.test.(ts|js)'],
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json',
-    },
+  extensionsToTreatAsEsm: ['.ts'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', {
+      useESM: true,
+      tsconfig: {
+        module: 'esnext',
+      },
+    }],
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
   },
 };
